2e413df0159a42886bab1d55c371abe028075e96
Use-Real <yuu.kuliah@gmail.com>
2024-11-01 11:19:33 +0700

    Update Data

--- database/db_antrian.sql
+++ database/db_antrian.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 13, 2021 at 03:17 AM
--- Server version: 5.7.34
--- PHP Version: 8.0.6
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2024 at 05:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(50) NOT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `password` varchar(72) NOT NULL,
+  `status` enum('admin','super admin') NOT NULL DEFAULT 'admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `nama_admin`, `email`, `password`, `status`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2y$10$PyyvQSH4OJ1CflSHjl.HJet4O5OntyCBqN8dAGef6onYb/iCu5AEK', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_antrian`
 --
 
@@ -33,11 +54,17 @@ CREATE TABLE `tbl_antrian` (
   `no_antrian` smallint(6) NOT NULL,
   `status` enum('1','0') NOT NULL DEFAULT '0',
   `updated_date` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Indexes for table `tbl_antrian`
@@ -48,6 +75,12 @@ ALTER TABLE `tbl_antrian`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_antrian`
